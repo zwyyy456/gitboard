7b424efa27813d9c446d29e049a5917a3320cf27
zwyyy456 <zwyyy456@hotmail.com>
2026-08-29 13:45:04 +0800

    feat(inspector): 管理详情缓存与并发加载

--- GitBoard/Models/ProjectItemDetail.swift
+++ GitBoard/Models/ProjectItemDetail.swift
@@ -12,3 +12,10 @@ struct ItemAuthor: Hashable, Sendable {
     let login: String
     let avatarURL: String?
 }
+
+enum ItemDetailState: Equatable {
+    case idle
+    case loading
+    case loaded(ProjectItemDetail)
+    case failed(String)
+}
